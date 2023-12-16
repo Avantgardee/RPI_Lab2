d228583c36c149083caf87730f77485b3dc5551f
LordN <119818933+LordNKey@users.noreply.github.com>
2023-12-16 20:39:44 +0300

    Idealism lost

--- src/components/AboutPage/AboutSite.jsx
+++ src/components/AboutPage/AboutSite.jsx
@@ -12,7 +12,11 @@ const AboutSite = () => {
           ? <SectionName name = "ABOUT PAGE"/>
           : <SectionName name = "O СТРАНИЦЕ"/>
       }
-      <TextInSection text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+      {
+        getLangOrSetDefault() === "En"
+          ? <TextInSection text = "Welcome to the page dedicated to the longest TV series in the history of cinema! Here, you'll find fascinating information about series that have captured audiences, holding their attention for endless hours. Utilize our convenient search page to easily find the series you're interested in. Each series has its own personalized page with a history, trailer, and event map, providing you with a comprehensive view of the captivating world of lengthy cinematic stories. Let's delve together into the exciting realm of the longest TV series, where every minute becomes a part of an incredible cinematic journey."/>
+          : <TextInSection text = "Добро пожаловать на страницу, посвященную самым длинным сериалам в истории кинематографа! Здесь вы найдете увлекательную информацию о сериалах, которые завоевали зрительские сердца, удерживая внимание на протяжении бескрайних часов. Используя нашу удобную страницу поиска, вы легко найдете интересующий вас сериал. Каждый сериал имеет свою персональную страницу с историей, трейлером и картой событий, предоставляя вам полный взгляд на захватывающий мир долгих кинематографических историй. Давайте вместе погрузимся в увлекательный мир самых продолжительных сериалов, где каждая минута становится частью невероятного кинематографического путешествия."/>
+      }    
     </div>
   );
 };
