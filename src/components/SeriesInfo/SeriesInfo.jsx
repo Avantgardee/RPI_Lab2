4f55226b09494e9aa2756ef760814faf1e519dfc
LordN <119818933+LordNKey@users.noreply.github.com>
2023-12-16 22:13:56 +0300

    Conflict 544554

--- src/components/SeriesInfo/SeriesInfo.jsx
+++ src/components/SeriesInfo/SeriesInfo.jsx
@@ -12,17 +12,6 @@ const SeriesInfo = (props) => {
             getLangOrSetDefault() === "En"
             ? (
               <>
-                <DescriptionLine nameOfLine="Год выпуска" info={props.year} />
-                <DescriptionLine nameOfLine="Страна производства" info={props.country} />
-                <DescriptionLine nameOfLine="Жанр" info={props.genre} />
-                <DescriptionLine nameOfLine="Сценаристы" info={props.writers} />
-                <DescriptionLine nameOfLine="Количество сезонов" info={props.seasons} />
-                <DescriptionLine nameOfLine="Продолжительность серии" info={props.time} />
-                <DescriptionLine nameOfLine="Описание" info={props.article} />
-              </>
-            )
-            : (                
-              <>
                 <DescriptionLine nameOfLine="Year of release" info={props.year} />
                 <DescriptionLine nameOfLine="Country of production" info={props.country} />
                 <DescriptionLine nameOfLine="Genre" info={props.genre} />
@@ -30,6 +19,17 @@ const SeriesInfo = (props) => {
                 <DescriptionLine nameOfLine="Number of seasons" info={props.seasons} />
                 <DescriptionLine nameOfLine="Series duration" info={props.time} />
                 <DescriptionLine nameOfLine="Description" info={props.article} />
+              </>
+            )
+            : (                
+              <>
+                <DescriptionLine nameOfLine="Год выпуска" info={props.year} />
+                <DescriptionLine nameOfLine="Страна производства" info={props.country} />
+                <DescriptionLine nameOfLine="Жанр" info={props.genre} />
+                <DescriptionLine nameOfLine="Сценаристы" info={props.writers} />
+                <DescriptionLine nameOfLine="Количество сезонов" info={props.seasons} />
+                <DescriptionLine nameOfLine="Продолжительность серии" info={props.time} />
+                <DescriptionLine nameOfLine="Описание" info={props.article} />
               </>
             )
         }
